979a2bc0f4206481f07053ca4d75b9f4d9fabc8e
Kiran <keer4n@gmail.com>
2018-10-18 02:15:24 +0000

    first commit

---
+++ src/sam/Main.java
@@ -0,0 +1,16 @@
+
+package sam;
+
+import java.math.BigInteger;
+
+public class Main {
+
+		public static void main(String [] args) {
+				BigInteger base = new BigInteger("12");
+				BigInteger exponent = new BigInteger("124");
+				
+				
+				System.out.println(exponent.toString(2));
+			
+		}
+}
